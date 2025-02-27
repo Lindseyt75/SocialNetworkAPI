3112905b628ebe8a2146b48aaa6f32a0792ee36b
Timothy Lindsey <lindseyt75@yahoo.com>
2025-02-26 23:54:45 -0500

    added develop files

--- dist/models/index.js
+++ dist/models/index.js
@@ -1,4 +1,1 @@
-import User from './User.js';
-import Thought from './Thought.js';
-import ReactionSchema from './Reaction.js';
-export { User, Thought, ReactionSchema };
+"use strict";
--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,5 +1,1 @@
-import { Router } from 'express';
-import apiRoutes from './api/index.js';
-const router = Router();
-router.use('/api', apiRoutes);
-export default router;
+"use strict";
